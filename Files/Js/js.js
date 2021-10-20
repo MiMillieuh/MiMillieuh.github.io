b0c8a68ed645f8b5167fca7ee2803fa870498afd
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-10-20 15:05:18 +0200

    update(wip)

--- Files/Js/js.js
+++ Files/Js/js.js
@@ -1,3 +1,9 @@
+function unloadload(){
+    document.getElementById('welcome').style.display = 'none';
+}
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(unloadload, 2000);
+})
 function pres(){
     document.getElementById('pres').className = 'presnone';
     document.getElementById('prestitle').style.display = 'none';
@@ -8,11 +14,28 @@ function secpagedis(){
     document.getElementById('btnpg').style.display = 'none';
     document.getElementById('btnpage').id = 'btnpagesec';
 }
+function presrst(){
+    document.getElementById('pres').className = '';
+    document.getElementById('prestitle').style.display = 'flex';
+    document.getElementById('prestxt').className = '';
+}
+function secpagedisrst(){
+    document.getElementById('titlesec').className = 'titlesecnodisplay';
+    document.getElementById('btnpg').style.display = 'flex';
+    document.getElementById('btnpagesec').id = 'btnpage';
+}
 function Secpage() {
+    if(document.getElementById('container')){
     document.getElementById('container').id = 'containersec';
     document.getElementById('pres').className = 'prestra';
     document.getElementById('btnpg').className = 'btnclicked';
     setTimeout(pres, 200);
     setTimeout(secpagedis, 1000);
-
+    }else{
+        document.getElementById('containersec').id = 'container';
+        document.getElementById('pres').className = 'prestra';
+        document.getElementById('btnpg').className = 'btnclickedtwo';
+        setTimeout(presrst, 200);
+        setTimeout(secpagedisrst, 1000);
+    }
 }
