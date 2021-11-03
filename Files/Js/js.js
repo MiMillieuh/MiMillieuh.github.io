42ba68fc50f7ef0e6e0b907c471a7f90c7c3fa76
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-11-03 20:03:40 +0100

    Fix Firefox not scrolling

--- Files/Js/js.js
+++ Files/Js/js.js
@@ -15,24 +15,34 @@ function secpagedis(){
     document.getElementById('btnpage').id = 'btnpagesec';
 }
 function presrst(){
+    document.getElementById('titlesec').className = 'titlesecnodisplay';
     document.getElementById('pres').className = '';
     document.getElementById('prestitle').style.display = 'flex';
     document.getElementById('prestxt').className = '';
 }
 function secpagedisrst(){
-    document.getElementById('titlesec').className = 'titlesecnodisplay';
     document.getElementById('btnpg').style.display = 'flex';
     document.getElementById('btnpagesec').id = 'btnpage';
 }
+/*
+$(document).bind('mousewheel', function(e) {
+    var delta = e.originalEvent.wheelDelta;
+    if(delta <= -1 && document.getElementById('container')){
+        setTimeout(Secpage, 0);
+    }
+ 
+ });
+ */   
 function Secpage() {
-    if(document.getElementById('container')){
-    document.getElementById('container').id = 'containersec';
+    if(document.getElementById('container').className == 'container'){
+    document.getElementById('container').className = 'containersec';
     document.getElementById('pres').className = 'prestra';
     document.getElementById('btnpg').className = 'btnclicked';
     setTimeout(pres, 200);
     setTimeout(secpagedis, 1000);
     }else{
-        document.getElementById('containersec').id = 'container';
+        urlreset = ""
+        document.getElementById('container').className = 'container';
         document.getElementById('pres').className = 'prestra';
         document.getElementById('btnpg').className = 'btnclickedtwo';
         setTimeout(presrst, 200);
