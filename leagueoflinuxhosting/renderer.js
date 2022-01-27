30f5990f172f8eff112d4ed36b8e13877076a310
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-01-27 17:10:07 +0100

    Update renderer.js

--- leagueoflinuxhosting/renderer.js
+++ leagueoflinuxhosting/renderer.js
@@ -23,39 +23,45 @@ function SetVolume(val)
 
 
 function PlayDL(){
-window.api.send("toMain", "install");
-document.getElementById("PlayDL").disabled = true
-let timerlolmin = 5
-let timerlolsec = 30
-let timerlolfull = 332
-document.getElementById("PlayDL").innerHTML = '<h5 id="timerlaunch">Launching...</h5><div class="progress"><div class="progress-value"></div></div>'
-setTimeout(Loadinglolloop, 1000);
-function Loadinglolloop(){
-    timerlolfull--
-    timerlolsec--
-if (timerlolsec == 0){
-    timerlolmin--
-    timerlolsec = 60
-}
-if (timerlolsec < 10){
-    document.getElementById("timerlaunch").innerHTML = timerlolmin + ":0" + timerlolsec
-}else{
-document.getElementById("timerlaunch").innerHTML = timerlolmin + ":" + timerlolsec
-}
-if(timerlolfull !=0){
-    setTimeout(Loadinglolloop, 1000);
-
-}else{
-    document.getElementById("timerlaunch").innerHTML =''
-}
-}
-}
-            //recieve from main
-
-        window.api.receive("fromMain", (data) => {
-            console.log(`Received ${data} from main process`);
-            if(data == "installed"){
-                document.getElementById("PlayDL").innerHTML = "Launched"
+    window.api.receive("fromMain", (data) => {
+        console.log(`Received ${data} from main process`);
+        if(data == "launching"){
+            window.api.send("toMain", "install");
+            document.getElementById("PlayDL").disabled = true
+            let timerlolmin = 5
+            let timerlolsec = 30
+            let timerlolfull = 332
+            document.getElementById("PlayDL").innerHTML = '<h5 id="timerlaunch">Launching...</h5><div class="progress"><div class="progress-value"></div></div>'
+            setTimeout(Loadinglolloop, 1000);
+            function Loadinglolloop(){
+                timerlolfull--
+                timerlolsec--
+            if (timerlolsec == 0){
+                timerlolmin--
+                timerlolsec = 60
             }
-        });
+            if (timerlolsec < 10){
+                document.getElementById("timerlaunch").innerHTML = timerlolmin + ":0" + timerlolsec
+            }else{
+            document.getElementById("timerlaunch").innerHTML = timerlolmin + ":" + timerlolsec
+            }
+            if(timerlolfull !=0){
+                setTimeout(Loadinglolloop, 1000);
+            
+            }else{
+                document.getElementById("timerlaunch").innerHTML =''
+            }
+            }
+            }
+                        //recieve from main
+            
+                    window.api.receive("fromMain", (data) => {
+                        console.log(`Received ${data} from main process`);
+                        if(data == "launched"){
+                            document.getElementById("PlayDL").innerHTML = "Launched"
+                        }
+                    });
+            
+        }
+    )};
 
